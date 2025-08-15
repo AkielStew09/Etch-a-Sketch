a20322361d3ef6e657bcc728d58d32c016a74908
Akiel Stewart <140494913+AkielStew09@users.noreply.github.com>
2025-08-14 23:54:47 -0500

    working on the new grid button. This is taking a while since I have to change the hardcoded length information to dynamic info that will work for all inputs

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-const renderSquares = ()=>{
-    let container = document.querySelector("#container");
-    for(let i = 0; i < 16; ++i){
+let newGridBtn = document.querySelector("#newGrid");
+newGridBtn.addEventListener("click", ()=>{
+    let amount = parseInt(prompt("New Grid! \nNumber of squares per side:"));
 
-        for(let i = 0; i < 16; ++i){
+});
+
+const renderSquares = (sides = 16)=>{
+    let container = document.querySelector("#container");
+    for(let i = 0; i < sides; ++i){
+
+        for(let i = 0; i < sides; ++i){
             let gridSquare = document.createElement("div");
             gridSquare.setAttribute("class", "gridSquare");
             
             container.appendChild(gridSquare);
         }
     }
+    let squares = document.querySelectorAll(".gridSquare");
+
+    squares.forEach((square)=>{
+        square.addEventListener("mouseenter", (e)=>{
+            e.target.classList.add("hover");
+        });
+    });
+    
+    
     
 }
 
