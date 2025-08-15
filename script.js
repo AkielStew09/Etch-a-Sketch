fc464bbe14309229711547c92705e26aa7441c64
Akiel Stewart <140494913+AkielStew09@users.noreply.github.com>
2025-08-15 00:29:05 -0500

    Changed the amount of sides so that it's more dynamic and can adjust to the users input, but still have some things to fix on it

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 let newGridBtn = document.querySelector("#newGrid");
 newGridBtn.addEventListener("click", ()=>{
-    let amount = parseInt(prompt("New Grid! \nNumber of squares per side:"));
+    let amount = parseInt(prompt("New Grid! \nNumber of squares per side (max. 100):"));
+    if(amount > 0 && amount <= 100){
+        renderSquares(amount);
+    } 
 
 });
 
 const renderSquares = (sides = 16)=>{
+
     let container = document.querySelector("#container");
+    container.innerHTML = "";
     for(let i = 0; i < sides; ++i){
 
         for(let i = 0; i < sides; ++i){
             let gridSquare = document.createElement("div");
             gridSquare.setAttribute("class", "gridSquare");
+            gridSquare.style.width = `calc(100% / ${sides})`;
+
             
             container.appendChild(gridSquare);
         }
