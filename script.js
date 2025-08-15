0a1e3c98647603b6e292f093b7b5c2d22ee4c579
Akiel Stewart <stewartakiel0@gmail.com>
2025-08-15 11:06:44 -0500

    Didn't make much changes, but added comments explaining what blocks of code were written to achieve what.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let newGridBtn = document.querySelector("#newGrid");
+
 newGridBtn.addEventListener("click", ()=>{
     let amount = parseInt(prompt("New Grid! \nNumber of squares per side (max. 100):"));
     if(amount > 0 && amount <= 100){
@@ -9,11 +10,17 @@ newGridBtn.addEventListener("click", ()=>{
 
 const renderSquares = (sides = 16)=>{
 
+    //retrieve container and initialise it to empty.
     let container = document.querySelector("#container");
     container.innerHTML = "";
+
+    //nested for loop, to feel more two dimensional idk. I'm used to doing it like this
     for(let i = 0; i < sides; ++i){
 
         for(let i = 0; i < sides; ++i){
+            //create square div, apply the class and set the width to exactly 
+            // take up the width with the calc function, dividing the 
+            //100% container width by the number of sides.
             let gridSquare = document.createElement("div");
             gridSquare.setAttribute("class", "gridSquare");
             gridSquare.style.width = `calc(100% / ${sides})`;
@@ -22,11 +29,14 @@ const renderSquares = (sides = 16)=>{
             container.appendChild(gridSquare);
         }
     }
+    //retrieve all the squares
     let squares = document.querySelectorAll(".gridSquare");
 
+    //iterates and adds the mouseenter event to each one
+    // which adds the hovered class.
     squares.forEach((square)=>{
         square.addEventListener("mouseenter", (e)=>{
-            e.target.classList.add("hover");
+            e.target.classList.add("hovered");
         });
     });
     
@@ -37,4 +47,4 @@ const renderSquares = (sides = 16)=>{
 
 
 
-renderSquares();
+//renderSquares();
